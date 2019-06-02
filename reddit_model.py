07cdb79ec1cce9884ef0b7670a1410416e11578d
Justin Chang <jchang0916@ucla.edu>
2019-06-02 01:23:10 -0700

    stack those cols

--- reddit_model.py
+++ reddit_model.py
@@ -137,82 +137,84 @@ def main(context):
     cs = cs.withColumn("grams", split(col("grams"), ",\s*").cast(ArrayType(StringType())).alias("grams"))
     cs = model.transform(cs)
     pos_result = pos_model.transform(cs)
-    neg_result = neg_model.transform(cs)
+    pos_result = pos_result.select('comment_score', 'submission_score','created_utc', 'title', 'state', 'link_id', udf(lambda x : 1 if x[1] > 0.2 else 0)('probability').alias('pos').cast(IntegerType()),col('rawPrediction').alias('pos_rawPrediction'),col('prediction').alias('pos_pred'), 'features')
+    t_result = neg_model.transform(pos_result)
 
-    pos_result = pos_result.select('link_id', 'created_utc', 'state', 'comment_score', 'submission_score', 'title', udf(lambda x : 1 if x[1] > 0.2 else 0)('probability').alias('pos').cast(IntegerType()))
-    neg_result = neg_result.select('link_id', 'created_utc', 'state', 'comment_score', 'submission_score', 'title', udf(lambda x : 1 if x[1] > 0.25 else 0)('probability').alias('neg').cast(IntegerType()))
+    t_result = t_result.select('comment_score', 'submission_score','created_utc', 'title', 'state', 'link_id', udf(lambda x : 1 if x[1] > 0.25 else 0)('probability').alias('neg').cast(IntegerType()),'pos',col('rawPrediction').alias('neg_rawPrediction'),col('prediction').alias('neg_pred'), 'pos_rawPrediction', 'pos_pred')
     #pos_result.show()
-    #neg_result.show()
+    #t_result.show()
 
     # TASK 10
     # Code for task 10
     import pyspark.sql.functions as func
     # 10.1
-    id_pos = pos_result.groupBy('link_id').agg(func.sum('pos').alias('count'))
-    id_neg = neg_result.groupBy('link_id').agg(func.sum('neg').alias('count'))
-    id_count = pos_result.groupBy('link_id').agg(func.count('*').alias('total'))
-    #id_pos.show()
-    #id_neg.show()
-    #id_count.show()
+    id_values = t_result.groupBy(col('link_id').alias('l_id')).agg(func.sum('pos').alias('count_pos'),func.sum('neg').alias('count_neg'))
+    id_count = t_result.groupBy('link_id').agg(func.count('*').alias('total'))
 
-    id_pos_ratio = id_pos.join(id_count, id_count.link_id == id_pos.link_id).withColumn('percentage', col('count') / col('total'))
-    id_neg_ratio = id_neg.join(id_count, id_count.link_id == id_neg.link_id).withColumn('percentage', col('count') / col('total'))
-    #id_pos_ratio.show()
-    #id_neg_ratio.show()
+    #id_count.show()
+    #id_values.show()
+
+    id_values_ratio = id_values.join(id_count, id_count.link_id == id_values.l_id).withColumn('Positive', col('count_pos') / col('total')).withColumn('Negative',col('count_neg')/col('total'))
+    id_values_ratio=id_values_ratio.select('link_id','Positive','Negative')
+    #id_values_ratio.show()
+    
+
+
 
     # 10.2
-    utc_pos = pos_result.groupBy(func.from_unixtime('created_utc', 'MM/dd/yyy').alias('date')).agg(func.sum('pos').alias('count'))
-    utc_neg = neg_result.groupBy(func.from_unixtime('created_utc', 'MM/dd/yyy').alias('date')).agg(func.sum('neg').alias('count'))
-    utc_count = pos_result.groupBy(func.from_unixtime('created_utc', 'MM/dd/yyy').alias('date')).agg(func.count('*').alias('total'))
-    #utc_pos.show()
-    #utc_neg.show()
+    utc_values = t_result.groupBy(func.from_unixtime('created_utc', 'yyyy/MM/dd').alias('d')).agg(func.sum('pos').alias('count_pos'),func.sum('neg').alias('count_neg'))
+    utc_count = t_result.groupBy(func.from_unixtime('created_utc', 'yyyy/MM/dd').alias('date')).agg(func.count('*').alias('total'))
+    
+    #utc_values.show()
     #utc_count.show()
 
-    utc_pos_ratio = utc_pos.join(utc_count, utc_count.date == utc_pos.date).withColumn('percentage', col('count') / col('total'))
-    utc_neg_ratio = utc_neg.join(utc_count, utc_count.date == utc_neg.date).withColumn('percentage', col('count') / col('total'))
-    #utc_pos_ratio.show()
-    #utc_neg_ratio.show()
+    utc_values_ratio = utc_values.join(utc_count, utc_count.date == utc_values.d).withColumn('Positive', col('count_pos') / col('total')).withColumn('Negative',col('count_neg')/col('total'))
+    utc_values_ratio=utc_values_ratio.select('date','Positive','Negative')
+    #utc_values_ratio.show() #works
+
 
     # 10.3
     states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-    state_pos = pos_result.filter(col('state').isin(states)).groupBy('state').agg(func.sum('pos').alias('count'))
-    state_neg = neg_result.filter(col('state').isin(states)).groupBy('state').agg(func.sum('neg').alias('count'))
-    state_count = pos_result.filter(col('state').isin(states)).groupBy('state').agg(func.count('*').alias('total'))
-    #state_pos.show()
-    #state_neg.show()
+    state_values = t_result.filter(col('state').isin(states)).groupBy(col('state').alias('s')).agg(func.sum('pos').alias('count_pos'),func.sum('neg').alias('count_neg'))
+    state_count = t_result.filter(col('state').isin(states)).groupBy('state').agg(func.count('*').alias('total'))
     #state_count.show()
 
-    state_pos_ratio = state_pos.join(state_count, state_count.state == state_pos.state).withColumn('percentage', col('count') / col('total'))
-    state_neg_ratio = state_neg.join(state_count, state_count.state == state_neg.state).withColumn('percentage', col('count') / col('total'))
-    #state_pos_ratio.show()
-    #state_neg_ratio.show()
+    state_values_ratio = state_values.join(state_count, state_count.state == state_values.s).withColumn('Positive', col('count_pos') / col('total')).withColumn('Negative',col('count_neg')/col('total'))
+    state_values_ratio=state_values_ratio.select('state','Positive','Negative')
+    #state_values_ratio.show() #this is good now
+
 
     # 10.4.comments_score
-    comment_score_pos = pos_result.groupBy('comment_score').agg(func.sum('pos').alias('count'))
-    comment_score_neg = neg_result.groupBy('comment_score').agg(func.sum('neg').alias('count'))
-    comment_score_count = pos_result.groupby('comment_score').agg(func.count('*').alias('total'))
-    #comment_score_pos.show()
-    #comment_score_neg.show()
+    comment_score_values = t_result.groupBy(col('comment_score').alias('c_score')).agg(func.sum('pos').alias('count_pos'),func.sum('neg').alias('count_neg'))
+    comment_score_count = t_result.groupby('comment_score').agg(func.count('*').alias('total'))
     #comment_score_count.show()
 
-    comment_score_pos_ratio = comment_score_pos.join(comment_score_count, comment_score_count.comment_score == comment_score_pos.comment_score).withColumn('percentage', col('count') / col('total'))
-    comment_score_neg_ratio = comment_score_neg.join(comment_score_count, comment_score_count.comment_score == comment_score_neg.comment_score).withColumn('percentage', col('count') / col('total'))
-    #comment_score_pos_ratio.show()
-    #comment_score_neg_ratio.show()
+    comment_score_values_ratio = comment_score_values.join(comment_score_count, comment_score_count.comment_score == comment_score_values.c_score).withColumn('Positive', col('count_pos') / col('total')).withColumn('Negative',col('count_neg')/col('total'))
+    comment_score_values_ratio=comment_score_values_ratio.select('comment_score','Positive','Negative')
+    #comment_score_values_ratio.show() #works
 
     # 10.4.submissions_score
-    submission_score_pos = pos_result.groupBy('submission_score').agg(func.sum('pos').alias('count'))
-    submission_score_neg = neg_result.groupBy('submission_score').agg(func.sum('neg').alias('count'))
-    submission_score_count = pos_result.groupby('submission_score').agg(func.count('*').alias('total'))
-    #submission_score_pos.show()
-    #submission_score_neg.show()
+    submission_score_values = t_result.groupBy(col('submission_score').alias('s_score')).agg(func.sum('pos').alias('count_pos'),func.sum('neg').alias('count_neg'))
+    submission_score_count = t_result.groupby('submission_score').agg(func.count('*').alias('total'))
     #submission_score_count.show()
 
-    submission_score_pos_ratio = submission_score_pos.join(submission_score_count, submission_score_count.submission_score == submission_score_pos.submission_score).withColumn('percentage', col('count') / col('total'))
-    submission_score_neg_ratio = submission_score_neg.join(submission_score_count, submission_score_count.submission_score == submission_score_neg.submission_score).withColumn('percentage', col('count') / col('total'))
-    #submission_score_pos_ratio.show()
-    #submission_score_neg_ratio.show()
+    submission_score_values_ratio = submission_score_values.join(submission_score_count, submission_score_count.submission_score == submission_score_values.s_score).withColumn('Positive', col('count_pos') / col('total')).withColumn('Negative',col('count_neg')/col('total'))
+    submission_score_values_ratio=submission_score_values_ratio.select('submission_score','Positive','Negative')
+    submission_score_values_ratio.show() #works
 
+
+
+    #PLOTS
+    utc_values_ratio.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("time_data.csv")
+    state_values_ratio.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("state_data.csv")
+    #need to filter top 10 for task 3 on plots
+    #here it is
+    submission_score_values_ratio.orderBy("Positive", ascending=False).limit(10).repartition(1).write.format("com.databricks.spark.csv").option("header","true").save("Top_Positive_Submissions.csv")
+    submission_score_values_ratio.orderBy("Negative", ascending=False).limit(10).repartition(1).write.format("com.databricks.spark.csv").option("header","true").save("Top_Negative_Submissions.csv") #idk if this is good
+
+    #last 2 csv
+    submission_score_values_ratio.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("submission_score.csv") #NOTE this should be doing something with like limit 10 based on spec. so dataframe should be limiting this, or i can limit it in between somewhere.
+    comment_score_values_ratio.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("comment_score.csv")
 
     
 
@@ -224,14 +226,3 @@ if __name__ == "__main__":
     sqlContext = SQLContext(sc)
     sc.addPyFile("cleantext.py")
     main(sqlContext)
-
-"""
-# CONVERT TO PDF
-Question 1:
-Input_id -> labeldem
-Input_id -> labelgop
-Input_id -> labeldjt
-Question 2:
-No the data is not fully normalized. We could decompose it further by moving all attributes relating to the author into another relation and using an author_id. Also, we could decompose it further by moving all attributes relating to the subreddit into another relation and only using the subreddit_id. 
-I think the data collector stored the data like this to make it easier to perform statistics on the data. It would be easier because we don't need to perform joins. Also, it may be that the data was made available in this format and so it was just collected as is. 
-"""
